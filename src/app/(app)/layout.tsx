2be086bed2261db899a21b4df36f15b88a270ee3
gus4214 <98795239+gus4214@users.noreply.github.com>
2025-04-19 01:12:12 +0900

    ✨ feat(nav-bar): simple design nav-bar

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Button } from '@/components/button';
+import MainNav from '@/components/main-nav';
+
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className='flex flex-1 flex-col min-h-screen'>
+			<div className='bg-background sticky inset-x-0 top-0 z-10 h-15 border-b' id='jibnadri_global_navigation_bar'>
+				<nav className='flex items-center justify-between relative isolate w-full h-full px-5' data-section-name='navigation_bar'>
+					<div>
+						<span className='font-bold'>집나들이</span>
+					</div>
+					<MainNav />
+					<Button size='sm'>로그인</Button>
+				</nav>
+			</div>
+			{children}
+		</div>
+	);
+};
+
+export default AppLayout;
---
+++ src/config/docs.ts
@@ -0,0 +1,12 @@
+import { NavItem } from '@/types/nav';
+
+export const MAIN_NAV: NavItem[] = [
+	{
+		title: '홈',
+		href: '/',
+	},
+	{
+		title: '지역 보기',
+		href: '/regions',
+	},
+];
